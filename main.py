6ebd22efc276446904561768d5c6bb548afe8364
hehuan2363 <35640983+hehuan2363@users.noreply.github.com>
2023-04-17 02:51:24 +0000

    Add changes

--- main.py
+++ main.py
@@ -2,21 +2,60 @@
 import streamlit as st
 from streamlit_chat import message
 
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain import OpenAI, VectorDBQA
+
+import os
+
+OPENAI_API_KEY =os.environ['OPENAI_API_KEY']
+PINECONE_API_KEY =os.environ['PINECONE_API_KEY']
+PINE_ENV = os.environ['PINE_ENV']
+
+model_name = 'text-embedding-ada-002'
+
+embed = OpenAIEmbeddings(
+    document_model_name=model_name,
+    query_model_name=model_name,
+    openai_api_key=OPENAI_API_KEY
+)
+
+from langchain.vectorstores import Pinecone
+
+text_field = "text"
+
+# switch back to normal index for langchain
+index = pinecone.Index(index_name)
+
+vectorstore = Pinecone(
+    index, embed.embed_query, text_field
+)
+
+
+
 
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
-    return chain
+    # llm = OpenAI(temperature=0)
+    # chain = ConversationChain(llm=llm)
+    llm = OpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+    )
+
+    qa = VectorDBQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        vectorstore=vectorstore
+    )
+    return qa
 
 chain = load_chain()
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+st.set_page_config(page_title="Ask for Canadian Policy", page_icon=":robot:")
+st.header("Ask for Canadian Policy")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -26,7 +65,7 @@ if "past" not in st.session_state:
 
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "", key="input")
     return input_text
 
 
